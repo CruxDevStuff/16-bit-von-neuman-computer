9bdbe98ae28ffad82f17ba99536038cb2fb080f3
crux <cruxdevy@gmail.com>
2024-04-30 14:26:53 +0530

    fully working and tested ALU!!!

--- hardware/blocks/ALU/alu.v
+++ hardware/blocks/ALU/alu.v
@@ -1,4 +1,4 @@
-module ALU (input [15:0] x, y, output reg[15:0] out, out2, result, input zx, nx, zy, ny, f, no, output zr, ng);
+module ALU (input wire signed[15:0] x, y, output reg signed[15:0] out, out2, result, input zx, nx, zy, ny, f, no, output reg zr, ng);
     reg [15:0] x_in; 
     reg [15:0] y_in; 
 
@@ -9,28 +9,22 @@ module ALU (input [15:0] x, y, output reg[15:0] out, out2, result, input zx, nx,
     always @(x or y) begin
         if (zx) begin
             x_in = {16{1'b0}}; 
-            // out = x_in; 
         end else begin
             x_in = x;  
-            // out = x_in; 
         end
 
         if (nx) begin 
             x_in = ~x_in; 
-            // out = x_in;
         end 
 
         if (zy == 1'b1) begin
             y_in = {16{1'b0}}; 
-            // out2 = y_in; 
         end else begin
             y_in = y;  
-            // out2 = y_in; 
         end
 
         if (ny) begin 
             y_in = ~y_in; 
-            // out2 = y_in;
         end 
     end
 
@@ -49,6 +43,22 @@ module ALU (input [15:0] x, y, output reg[15:0] out, out2, result, input zx, nx,
             result = adder_out; 
         end else begin
             result = and_out; 
+        end
+
+        if (no) begin
+            result = ~result; 
+        end
+
+        if (result == {16{1'b0}}) begin
+            zr = 1; 
+        end else begin
+            zr = 0; 
+        end
+
+        if (result < 0) begin
+            ng = 1; 
+        end else begin
+            ng = 0; 
         end
     end
 
