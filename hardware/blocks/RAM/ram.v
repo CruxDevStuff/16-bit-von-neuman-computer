833435d807970e22df3502a292b9150c3899fc51
crux <cruxdevy@gmail.com>
2024-05-02 00:06:44 +0530

    Add 1 bit and 16 bit memory devices

--- hardware/blocks/RAM/ram.v
+++ hardware/blocks/RAM/ram.v
@@ -1,17 +1,41 @@
-module top (
+// 1 and 16 bit memory devices that resemble a D-Flip Flop. 
+
+module REGISTER (
   input clk,
   input reset_n,
-  output [23:0] io_led
+  input d_in, 
+  output d_out
 );
 
-  reg [47:0] counter;
+  reg bit_data; 
 
   always @(posedge clk) begin
-    if (!reset_n)
-      counter <= 0;
-    else
-      counter <= counter + 1;
+    if (!reset_n) begin
+      bit_data <= 0; 
+    end else begin
+      bit_data <= d_in; 
+    end
   end
+  
+  assign d_out = bit_data; 
+endmodule
 
-  assign io_led = counter[47:24];
+module REGISTER_16(
+  input clk,
+  input reset_n,
+  input [15:0] d_in, 
+  output [15:0] d_out
+);
+
+  reg [15:0] bit_data; 
+
+  always @(posedge clk) begin
+    if (!reset_n) begin
+      bit_data <= {16{1'b0}}; 
+    end else begin
+      bit_data <= d_in; 
+    end
+  end
+  
+  assign d_out = bit_data; 
 endmodule
---
+++ hardware/blocks/RAM/ram_tb.v
@@ -0,0 +1,41 @@
+// 1 bit and 16 bit memory test benches. 
+
+`timescale 1ns/100ps // define the minimum time unit used by the simulation
+
+module ram_tb;
+    reg _clk=0; 
+    wire clk; 
+    reg reset_n = 1; 
+
+    realtime capture = 0.0;
+
+    always #5 _clk=~_clk; // invert clock every 5 ns
+    assign clk = _clk; 
+
+    reg data_in = 0; 
+    wire data_out; 
+    REGISTER register_1bit(clk, reset_n, data_in, data_out); 
+
+    reg [15:0] data_in16 = {16{1'b0}}; 
+    wire [15:0] data_out16; 
+    REGISTER_16 register_16(clk, reset_n, data_in16, data_out16); 
+
+    initial begin
+        forever begin
+            #2; 
+            data_in = ~data_in; 
+            data_in16 = ~data_in16; 
+            // capture = $realtime;
+            // $display("%t", capture); // uncomment to print the cycle count of the simulation in realtime.
+        end
+    end
+
+    initial begin
+        $display("RAM TESTBENCH"); 
+        $dumpfile("ram-dump.vcd");
+        $dumpvars(3);
+        #100000 // run simulation for 100000 timeunits (in this case 100000 ns as defined on top)
+        $finish;
+    end 
+
+endmodule
