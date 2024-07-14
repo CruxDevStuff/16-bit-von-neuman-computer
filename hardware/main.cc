504856b23df626b250e930792158a805307b57c7
crux <cruxdevy@gmail.com>
2024-07-14 11:35:28 +0530

    add imgui

--- hardware/main.cc
+++ hardware/main.cc
@@ -1,12 +1,19 @@
 #include <iostream>
+#include "include/main.h"
+
 #include "SDL.h"
 #include "SDL_timer.h"
 #include "SDL_ttf.h"
+#include "SDL_opengl.h"
+
+#include "include/imgui/imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+
 #include <verilated.h>
 #include "verilated_vcd_c.h"
 #include "Vcomputer.h"
 #include "Vcomputer___024root.h"
-#include "include/main.h"
 
 const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
 Vcomputer * computer_block = new Vcomputer;
@@ -18,7 +25,13 @@ SDL_Renderer * renderer = NULL;
 SDL_Texture * texture = NULL; 
 SDL_Surface * surface = NULL; 
 
+char * glsl_version = NULL;
+ImGuiIO * io = NULL;
+SDL_GLContext gl_context = NULL;
+
 int pressed_key = 0; 
+
+ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 int create_sdl_window() {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -26,37 +39,54 @@ int create_sdl_window() {
         return -1;
     }
 
+    #if defined(__APPLE__)
+        // GL 3.2 Core + GLSL 150
+        glsl_version = "#version 150";
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    #else
+        // GL 3.0 + GLSL 130
+        glsl_version = "#version 130";
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    #endif
+    
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+
     window = SDL_CreateWindow("Hack Computer",
                             SDL_WINDOWPOS_CENTERED,
                             SDL_WINDOWPOS_CENTERED,
                             SCREEN_WIDTH, SCREEN_HEIGHT,
-                            0);
+                            window_flags);
     if (!window) {
         std::cout << "Failed to create window\n";
         return -1;
     }
 
-    renderer = SDL_CreateRenderer(window, -1,
-                                SDL_RENDERER_ACCELERATED 
-                                | SDL_RENDERER_PRESENTVSYNC);
-    if (!renderer) {
-        printf("Failed to create renderer: %s\n", SDL_GetError());
-        return 1;
-    }
+    gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1); // enable vsync
 
-    surface = SDL_CreateRGBSurfaceWithFormat(SDL_SWSURFACE,
-                                                SCREEN_WIDTH, 
-                                                SCREEN_HEIGHT, 1, 
-                                                SDL_PIXELFORMAT_INDEX1MSB); 
-    if(!surface) {
-        std::cout << "Failed to get the surface from the window\n";
-        return -1; 
-    }
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    io = &ImGui::GetIO(); 
+    (void)io;
 
-    SDL_Color colors[2] = {{0, 0, 0, 255}, {255, 255, 255, 255}};
-    SDL_SetPaletteColors(surface->format->palette, colors, 0, 2);
+    // Set theme 
+    ImGui::StyleColorsDark();
 
-    SDL_UpdateWindowSurface(window);
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 
     return 0;
 }
@@ -152,7 +182,7 @@ int poll_sim_state() {
     
     while(!quit) {
         tick_clock(computer_block); 
-        map_memory_to_screen();
+        // map_memory_to_screen();
 
         computer_block->kb_in = pressed_key;
 
@@ -167,6 +197,8 @@ int poll_sim_state() {
         SDL_StartTextInput();
 
 		while(SDL_PollEvent(&event_handler) != 0) {
+            ImGui_ImplSDL2_ProcessEvent(&event_handler);
+
             pressed_key = 0;
             // do things according to user input type
             if(event_handler.type == SDL_QUIT) {
@@ -181,19 +213,41 @@ int poll_sim_state() {
                 // TODO: not text keys like : enter, backspace handled here.
             }
         } 
-    
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        bool show_demo_window = true;
+        ImGui::ShowDemoWindow(&show_demo_window);
+
         // std::cout << pressed_key; 
         std::cout << "---------" << std::endl;
         
+        // Rendering
+        ImGui::Render();
+        glViewport(0, 0, (int)io->DisplaySize.x, (int)io->DisplaySize.y);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+
         SDL_StopTextInput();
-        SDL_RenderClear(renderer);
-        SDL_Texture * screen_texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_RenderCopy(renderer, screen_texture, NULL, NULL);
-        SDL_RenderPresent(renderer);
-        SDL_DestroyTexture(screen_texture); 
+      
 	}
     
     end_dump(); 
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0; 
 }
